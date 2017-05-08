d2ce0736e1927f951e053ec6bda160df73b65551
SlicewOw <slicewow@myrisk-gaming.de>
2017-05-08 09:33:19 +0200

    Update

--- WebApp/src/app/common/auth.guard.ts
+++ WebApp/src/app/common/auth.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
-import { tokenNotExpired } from 'angular2-jwt';
 
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private router: Router) {}
 
+/*
   canActivate() {
     // Check to see if a user has a valid JWT
     if (tokenNotExpired()) {
@@ -18,5 +18,5 @@ export class AuthGuard implements CanActivate {
     this.router.navigate(['/login']);
     return false;
   }
-
+*/
 }
